24feab3bd8e1757400aeaef18b1257e8c50772ab
chandhureddy <chandhureddy>
2025-03-10 15:58:47 +0530

    txt

---
+++ one.py
@@ -0,0 +1,202 @@
+# import platform
+
+# x = dir(platform)
+# print(x)
+
+
+
+
+
+# def greeting(name):
+#       print("Hello, " + name)
+
+# person1 = {
+#   "name": "John",
+#   "age": 36,
+#   "country": "Norway"
+# }
+
+
+
+
+
+# from mymodule import person1
+
+# print (person1["age"])
+
+
+
+
+
+
+# x = "Hello World!"
+
+# print(len(x))
+
+
+
+
+
+# mytuple = ("apple", "banana", "cherry")
+
+# print(len(mytuple))
+
+
+
+
+
+# class Car:
+#       def __init__(self, brand, model):
+#     self.brand = brand
+#     self.model = model
+
+#   def move(self):
+#     print("Drive!")
+
+# class Boat:
+#   def __init__(self, brand, model):
+#     self.brand = brand
+#     self.model = model
+
+#   def move(self):
+#     print("Sail!")
+
+# class Plane:
+#   def __init__(self, brand, model):
+#     self.brand = brand
+#     self.model = model
+
+#   def move(self):
+#     print("Fly!")
+
+# boat1 = Boat("Ibiza", "Touring 20") #Create a Boat object
+# plane1 = Plane("Boeing", "747")     #Create a Plane object
+
+# for x in (boat1, plane1):
+#   x.move()
+
+
+
+
+
+# thistuple = ("apple",)
+# print(type(thistuple))
+
+
+# thistuple = ("apple")
+# print(type(thistuple))
+
+
+
+
+
+# car = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+
+
+
+
+
+# car.pop("brand")
+# car.pop("model")
+# car.pop("year")
+# print(car)
+# print(car)
+# print(car)
+
+
+
+
+# import keyword
+# print(keyword.kwlist)
+# #how many keywords
+# print(len(keyword.kwlist))
+
+
+
+
+# map = 'chandu'
+# filter = 'ccd coffee'
+# print(map)
+# print(filter)
+
+
+
+
+# def my_function(fname):
+#       print(fname + " Refsnes")
+
+# my_function("Emil")
+# my_function("Tobias")
+# my_function("Linus")
+
+
+
+
+# def my_function(fname, lname):
+#       print(fname + " " + lname)
+
+# my_function("Email", "Referenses")
+
+
+
+
+
+# thisset = {"apple", "banana", "cherry", False, True, 0}
+
+# print(thisset)
+
+
+
+
+# print(10 > 9)
+# print(10 == 9)
+# print(10 < 9)
+
+
+
+
+# eid=101
+# esal=45000.45
+# ename="Rahul"
+# avail='N'
+# c = 10+20j
+# discount=True 
+
+# eids=[101,102,103,104]
+# enames=('Rahul','Sonia','Priyanka')
+# numbers={10,20,30,10,20,30}
+# emp={
+#     'eid':101,
+#     'ename':'Rahul'
+# }
+# #print Data type
+# print(type(eid))
+# print(type(esal))
+# print(type(ename))
+# print(type(avail))
+# print(type(c))
+# print(type(discount))
+# print(type(eids))
+# print(type(enames))
+# print(type(numbers))
+# print(type(emp))
+
+
+
+
+# bytes_data = bytes([0,10,20,255])
+# print(bytes_data)
+# print(type(bytes_data))
+# for data in bytes_data:
+#     print(data)
+
+
+
+
+
+
+
